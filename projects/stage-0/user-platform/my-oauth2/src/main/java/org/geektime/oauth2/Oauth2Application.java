59479775cccde5c9450cf2eaa6061568fd8da1aa
zhk <thurston1982@gmail.com>
2021-04-21 14:26:35 +0800

    bugfix

--- projects/stage-0/user-platform/my-oauth2/src/main/java/org/geektime/oauth2/Oauth2Application.java
+++ projects/stage-0/user-platform/my-oauth2/src/main/java/org/geektime/oauth2/Oauth2Application.java
@@ -2,10 +2,21 @@ package org.geektime.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
-public class SocialApplication extends WebSecurityConfigurerAdapter {
+public class Oauth2Application extends WebSecurityConfigurerAdapter {
 
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
@@ -28,7 +39,7 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SocialApplication.class, args);
+        SpringApplication.run(Oauth2Application.class, args);
     }
 
 }
