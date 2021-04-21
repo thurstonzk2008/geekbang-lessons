5708fbe1f2ec5c26185a5e7be45be19c7ad529ea
zhk <thurston1982@gmail.com>
2021-04-21 11:01:13 +0800

    add oauth2

---
+++ projects/stage-0/user-platform/oauth2/src/main/java/org/geektime/oauth2/Oauth2Application.java
@@ -0,0 +1,34 @@
+package org.geektime.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+        // @formatter:on
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+
+}
