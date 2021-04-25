468e24248cae6984bf2a58cad8d1c9dac60b0fea
zhk <thurston1982@gmail.com>
2021-04-25 13:46:18 +0800

    Update OauthController.java

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/OauthController.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/OauthController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
+import org.geektimes.configuration.microprofile.config.DefaultConfigProviderResolver;
 import org.geektimes.projects.user.web.utils.HttpURLClient;
 import org.geektimes.projects.user.web.utils.URLParamsUtil;
 import org.geektimes.web.mvc.controller.Controller;
@@ -27,6 +28,8 @@ public class OauthController implements Controller {
 
     //    private final ConfigProviderResolver provider = ConfigProviderResolver.instance();
 //    private final Config providerConfig = provider.getConfig();
+    private final ConfigProviderResolver provider = new DefaultConfigProviderResolver();
+    private final  Config config = provider.getConfig();
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     private static final String GET_TOKEN_API = "https://github.com/login/oauth/access_token";
@@ -40,7 +43,7 @@ public class OauthController implements Controller {
     @GET
     @Path("/login")
     public String login(HttpServletRequest request, HttpServletResponse response) {
-//        String client_id = providerConfig.getValue(CLIENT_ID_KEY, String.class);
+        String client_id = config.getValue(CLIENT_ID_KEY, String.class);
         request.setAttribute("client_id", client_id);
         return "login-oauth.jsp";
     }
@@ -60,7 +63,7 @@ public class OauthController implements Controller {
 
         Map<String, String> resultMap = URLParamsUtil.resolveParamsByUrl("?" + result);
         String token = resultMap.get("access_token");
-//
+
 //        // 获取个人信息
         String userInfo = HttpURLClient.doGetWithToken(GET_USER_INFO_API, token);
 
@@ -75,11 +78,7 @@ public class OauthController implements Controller {
 
         String name  = actualObj.get("name").textValue();
         String avatar_url = actualObj.get("avatar_url").textValue();
-//        logger.info("user info is: " + userInfo);
-//
-//        JSONObject userObject = JSONObject.parseObject(userInfo);
-//        String name = userObject.getString("name");
-//        String avatar_url = userObject.getString("avatar_url");
+
         request.setAttribute("avatar_url", avatar_url);
         request.setAttribute("name", name);
 
